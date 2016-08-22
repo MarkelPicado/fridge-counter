aa39de1ddc2f3d52925207e11082f7d94ef82d4a
mpicado <mpicado@s21sec.com>
2016-08-22 22:46:54 +0200

    Added MILK

---
+++ src/magnet/milk.py
@@ -0,0 +1,36 @@
+###############################################################################
+###############################################################################
+###############################################################################
+
+''' 
+En este modulo se define todo lo referente a la clase fridge
+'''
+
+__author__      = "Nombre"                                               
+__credits__     = ["Markel Picado Ortiz"]                                                         
+__version__     = "0.0.1"                                                          
+__maintainer__  = "Markel Picado Ortiz"                                         
+__email__       = "mpicado001@gmail.com"                                          
+__status__      = "Work In Progress"                                                   
+__date__        = "22/08/2016"
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'src')) # */FridgeCounter/src
+
+from magnet import Magnet
+from magnet import MILK
+from product import GENERIC
+
+class Milk(Magnet):
+	
+	magnet_type = GENERIC
+	magnet_name = MILK
+
+	def __init__(self, magnet_barcode=None):
+		super(Milk, self).__init__(magnet_barcode)
